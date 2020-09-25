9bcfa36854498fb3fe7b6b6255a1011e128a9c07
jsi-webdev <j.skullow@gmail.com>
2020-09-26 03:17:24 +0900

    R2 - Add Event : implement function to calculate Loan interest

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', function(e){
+document.getElementById("loan-form").addEventListener("submit", function (e) {
   // Hide results
-  document.getElementById('results').style.display = 'none';
-  
+  document.getElementById("results").style.display = "none";
+
   // Show loader
-  document.getElementById('loading').style.display = 'block';
+  document.getElementById("loading").style.display = "block";
 
   setTimeout(calculateResults, 2000);
 
@@ -12,3 +12,33 @@ document.getElementById('loan-form').addEventListener('submit', function(e){
 });
 
 // Calculate Results
+function calculateResults() {
+  console.log("Calculating...");
+  // UI Vars
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show results
+    document.getElementById("results").style.display = "block";
+    // Hide loader
+    document.getElementById("loading").style.display = "none";
+  } else {
+  }
+}
