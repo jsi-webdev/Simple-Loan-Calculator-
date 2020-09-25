55bd7f67b18f5a0b4691d7ed0f4a74b85dd9e472
jsi-webdev <j.skullow@gmail.com>
2020-09-26 03:02:38 +0900

    R2 - Add Event : form submit & loading ui show up

---
+++ app.js
@@ -0,0 +1,14 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
+
+// Calculate Results
